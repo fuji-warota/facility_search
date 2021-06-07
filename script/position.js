d8a4905fe91bc23b81953f0b41919c7f91feb1b5
Fuji_warota <255116p@gs.kochi-tech.ac.jp>
2021-06-08 00:34:44 +0900

    jsで取得した位置情報をphpに渡そうと試みた

--- script/position.js
+++ script/position.js
@@ -5,6 +5,14 @@ function success(pos) {
   console.log(`緯度: ${crd.latitude}`);
   console.log(`経度: ${crd.longitude}`);
   console.log(`精度: ${crd.accuracy} meters`);
+  var pos = {'lat':`crd.latitude`,'lon':`crd.longitude`};
+  $.ajax({
+      type: 'POST',
+      url: './index.php',
+      data: pos,
+      }).fail(function){
+          window.alert('位置情報送信失敗');
+        });
 }
 
 function error(err) {
