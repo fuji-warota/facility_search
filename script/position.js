e933b58e2d4b2f8f9aac23b21f87fea44cb58bf0
Fuji_warota <255116p@gs.kochi-tech.ac.jp>
2021-06-09 00:26:54 +0900

    位置情報を取得できない原因を調査した

--- script/position.js
+++ script/position.js
@@ -1,18 +1,12 @@
 function success(pos) {
   var crd = pos.coords;
 
-  console.log('success!:');
-  console.log(`緯度: ${crd.latitude}`);
-  console.log(`経度: ${crd.longitude}`);
-  console.log(`精度: ${crd.accuracy} meters`);
-  var pos = {'lat':`crd.latitude`,'lon':`crd.longitude`};
+  var $map = {'lat':`${crd.latitude}`,'lon':`${crd.longitude}`};
   $.ajax({
       type: 'POST',
       url: './index.php',
-      data: pos,
-      }).fail(function){
-          window.alert('位置情報送信失敗');
-        });
+      data: $map,
+      });
 }
 
 function error(err) {
